dcf6545875d74c81f6311c5e7469427cb756028b
senyzy <senyzy@foxmail.com>
2022-02-16 17:41:05 +0800

    修改快递100位即时查询

--- lib/Trackers/Kuaidi100.php
+++ lib/Trackers/Kuaidi100.php
@@ -2,6 +2,7 @@
 
 namespace Ttshop\Express\Trackers;
 
+use Curl\Curl;
 use Ttshop\Express\Exceptions\TrackingException;
 use Ttshop\Express\Status;
 use Ttshop\Express\Waybill;
@@ -9,6 +10,10 @@ use Ttshop\Express\Waybill;
 class Kuaidi100 extends BaseTracker implements TrackerInterface
 {
     use TrackerTrait;
+
+    public $Customer;
+
+    public $AppKey;
 
     public static function getSupportedExpresses()
     {
@@ -20,12 +25,13 @@ class Kuaidi100 extends BaseTracker implements TrackerInterface
             '百福东方' => 'baifudongfang',
             'BHT' => 'bht',
             '邦送' => 'bangsongwuliu',
-            '希伊艾斯（CCES）' => 'cces',
+            'CCES-国通' => 'cces',
             '中国东方（COE）' => 'coe',
             '传喜' => 'chuanxiwuliu',
             '加拿大邮政' => 'canpost',
             '大田' => 'datianwuliu',
             '德邦' => 'debangwuliu',
+            '德邦' => 'debangkuaidi',
             'DPEX' => 'dpex',
             'DHL' => 'dhl',
             'DHL国际' => 'dhlen',
@@ -36,103 +42,86 @@ class Kuaidi100 extends BaseTracker implements TrackerInterface
             'EMS英文' => 'emsen',
             'EMS国际' => 'emsguoji',
             'EMS国际英文' => 'emsinten',
-            'Fedex英文' => 'fedex',
-            'Fedex' => 'fedexcn',
+            'Fedex国际' => 'fedex',
+            'Fedex国际(中文)' => 'fedexcn',
             'Fedex美国' => 'fedexus',
+            'Fedex英国' => 'fedexuk',
+            'Fedex英国(中文)' => 'fedexukcn',
             '飞康达' => 'feikangda',
             '飞快达' => 'feikuaida',
-            '凡客如风达' => 'rufengda',
-            '风行天下' => 'fengxingtianxia',
             '飞豹' => 'feibaokuaidi',
-            '港中能达' => 'ganzhongnengda',
+            '能达' => 'ganzhongnengda',
             '国通' => 'guotongkuaidi',
             '广东邮政' => 'guangdongyouzhengwuliu',
             'GLS' => 'gls',
             '共速达' => 'gongsuda',
-            '汇通' => 'huitongkuaidi',
             '汇强' => 'huiqiangkuaidi',
-            '华宇' => 'tiandihuayu',
             '恒路' => 'hengluwuliu',
-            '华夏龙' => 'huaxialongwuliu',
-            '海航天天' => 'tiantian',
             '海外环球' => 'haiwaihuanqiu',
-            '河北建华' => 'hebeijianhua',
             '海盟' => 'haimengsudi',
             '华企' => 'huaqikuaiyun',
-            '山东海红' => 'haihongwangsong',
+            '海红网送' => 'haihongwangsong',
+            '京东' => 'jd',
+            '极兔' => 'jtexpress',
             '佳吉' => 'jiajiwuliu',
             '佳怡' => 'jiayiwuliu',
             '加运美' => 'jiayunmeiwuliu',
             '京广' => 'jinguangsudikuaijian',
             '急先达' => 'jixianda',
             '晋越' => 'jinyuekuaidi',
-            '捷特' => 'jietekuaidi',
             '金大' => 'jindawuliu',
             '嘉里大通' => 'jialidatong',
             '快捷' => 'kuaijiesudi',
-            '康力' => 'kangliwuliu',
             '跨越' => 'kuayue',
             '联昊通' => 'lianhaowuliu',
-            '龙邦' => 'longbanwuliu',
             '蓝镖' => 'lanbiaokuaidi',
-            '乐捷递' => 'lejiedi',
             '联邦' => 'lianbangkuaidi',
             '联邦英文' => 'lianbangkuaidien',
             '立即送' => 'lijisong',
             '隆浪' => 'longlangkuaidi',
-            '门对门' => 'menduimen',
             '美国' => 'meiguokuaidi',
             '明亮' => 'mingliangwuliu',
             'OCS' => 'ocs',
             'OnTrac' => 'ontrac',
-            '全晨' => 'quanchenkuaidi',
             '全际通' => 'quanjitong',
             '全日通' => 'quanritongkuaidi',
             '全一' => 'quanyikuaidi',
             '全峰' => 'quanfengkuaidi',
-            '七天连锁' => 'sevendays',
-            '如风达' => 'rufengda',
-            '国内邮件' => 'youzhengguonei',
-            '国际邮件' => 'youzhengguoji',
             '申通' => 'shentong',
             '顺丰' => 'shunfeng',
-            '顺丰（英文结果）' => 'shunfengen',
+            '顺丰冷链' => 'shunfenglengyun',
+            '顺丰-荷兰' => 'shunfengnl',
+            '顺丰-香港' => 'shunfenghk',
             '三态' => 'santaisudi',
             '盛辉' => 'shenghuiwuliu',
             '速尔' => 'suer',
-            '盛丰' => 'shengfengwuliu',
             '上大' => 'shangda',
-            '三态' => 'santaisudi',
-            '山东海红' => 'haihongwangsong',
             '赛澳递' => 'saiaodi',
-            '山西红马甲' => 'sxhongmajia',
+            '红马甲' => 'sxhongmajia',
             '圣安' => 'shenganwuliu',
             '穗佳' => 'suijiawuliu',
-            '天地华宇' => 'tiandihuayu',
             '天天' => 'tiantian',
+            '天地华宇' => 'tiandihuayu',
             'TNT（中文结果）' => 'tnt',
             'TNT（英文结果）' => 'tnten',
             '通和天下' => 'tonghetianxia',
             'UPS（中文结果）' => 'ups',
-            'UPS（英文结果）' => 'upsen',
-            '优速' => 'youshuwuliu',
+            'UPS（全球件）' => 'upsen',
             'USPS（中英文）' => 'usps',
             '万家' => 'wanjiawuliu',
             '万象' => 'wanxiangwuliu',
             '微特派' => 'weitepai',
-            '新邦' => 'xinbangwuliu',
             '信丰' => 'xinfengwuliu',
-            '新邦' => 'xinbangwuliu',
-            '新蛋奥硕' => 'neweggozzo',
             '香港邮政' => 'hkpost',
+            '邮政' => 'youzhengguonei',
+            '邮政国际' => 'youzhengguoji',
             '圆通' => 'yuantong',
             '韵达' => 'yunda',
-            '运通' => 'yuntongkuaidi',
+            '运通中港' => 'yuntongkuaidi',
             '远成' => 'yuanchengwuliu',
             '亚风' => 'yafengsudi',
             '一邦' => 'yibangwuliu',
             '优速' => 'youshuwuliu',
-            '源伟丰' => 'yuanweifeng',
             '元智捷诚' => 'yuanzhijiecheng',
             '越丰' => 'yuefengwuliu',
             '源安达' => 'yuananda',
@@ -140,29 +129,38 @@ class Kuaidi100 extends BaseTracker implements TrackerInterface
             '忠信达' => 'zhongxinda',
             '芝麻开门' => 'zhimakaimen',
             '银捷' => 'yinjiesudi',
-            '一统飞鸿' => 'yitongfeihong',
             '中通' => 'zhongtong',
             '宅急送' => 'zhaijisong',
             '中邮' => 'zhongyouwuliu',
-            '忠信达' => 'zhongxinda',
-            '中速快件' => 'zhongsukuaidi',
-            '芝麻开门' => 'zhimakaimen',
-            '郑州建华' => 'zhengzhoujianhua',
+            '中速' => 'zhongsukuaidi',
             '中天万运' => 'zhongtianwanyun',
-            '邮政' => 'youzhengguonei',
-            '邮政国际' => 'youzhengguoji',
         ];
     }
 
     public function track(Waybill $waybill)
     {
-        $apiUrl = 'http://www.kuaidi100.com/query?type='
-            . urlencode(static::getExpressCode($waybill->express))
-            . '&postid='
-            . urlencode($waybill->id);
-        $curl = static::httpGet($apiUrl);
+        $curl = new Curl();
+        $curl->setHeader('Content-Type','application/x-www-form-urlencoded');
+        $requestData = [
+            'customer' => $this->Customer,
+            'param' => [
+                'com' => static::getExpressCode($waybill->express),
+                'num' => $waybill->id,
+                'resultv2' => '6'
+            ],
+        ];
+        if ($requestData['param']['com'] == 'shunfeng') {
+            $requestData['param']['phone'] = $waybill->mobile;
+        }
+        $requestData['param'] = json_encode($requestData['param'],320);
+        $requestData['sign'] = strtoupper(md5( $requestData['param'].$this->AppKey.$this->Customer));
+        $requestDataSub = '';
+        foreach ($requestData as $k => $v) {
+            $requestDataSub .= $k."=".urlencode($v)."&";
+        }
+        $requestDataSub = substr($requestDataSub,0,-1);
+        $curl->post('https://poll.kuaidi100.com/poll/query.do',$requestDataSub);
         $response = static::getJsonResponse($curl);
-
         if ($response->status != 200) {
             throw new TrackingException($response->message, $response);
         }
